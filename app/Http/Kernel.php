75da9d424cab7c73326264d47aba6433f1f9fe06
zabeer001 <binzabirtareq@gmail.com>
2025-03-30 10:09:35 +0600

    e

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,9 +41,10 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \App\Http\Middleware\HeaderMiddleware::class, 
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\HeaderMiddleware::class, 
+        
         ],
     ];
 
